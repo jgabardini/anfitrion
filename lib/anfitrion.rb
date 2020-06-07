38eed7b46ae4c8211736f0824c50e74016510fde
Juan Gabardini <jgabardini@computer.org>
2020-06-07 19:22:15 +0000

    Resultado demo viernes 5 junio 2020

---
+++ lib/anfitrion.rb
@@ -0,0 +1,16 @@
+class Anfitrion
+    def initialize idioma='es'
+        @idioma= idioma
+    end
+    def saluda invitado=''
+        saludos= {'es'=> 'Hola', 'en'=> 'Hello'}
+        if @idioma == 'en'
+            "Hello #{invitado}"
+        else
+            "Hola #{invitado}"
+        end
+       saludos[@idioma] + ' ' + invitado 
+    end
+
+
+end
